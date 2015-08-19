97030beee666d3fa0809209a34cfcca5662d8888
cynthiaechen <cynthiaechen@gmail.com>
2015-08-19 16:17:35 -0700

    Made functional pattern queue pass

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var insertIndex = 0;
+  var removeIndex = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[insertIndex] = value;
+    insertIndex ++;
   };
 
   someInstance.dequeue = function(){
+    var frontElement = storage[removeIndex];
+    delete storage[removeIndex];
+    removeIndex ++;
+    return frontElement;
   };
 
   someInstance.size = function(){
+    var count = 0;
+    for (var key in storage) {
+      count++;
+    }
+    console.log(storage);
+    return count;
   };
 
   return someInstance;
