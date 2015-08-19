f59aada6c6ac61ba2b5bed9e4e16ceac6a3fbe09
cynthiaechen <cynthiaechen@gmail.com>
2015-08-19 16:41:18 -0700

    Made functional stack pass

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,30 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var insertIndex = 0;
+  var removeIndex;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[insertIndex] = value;
+    insertIndex ++;
   };
 
   someInstance.pop = function(){
+    removeIndex = someInstance.size() - 1;
+    var backElement = storage[removeIndex];
+    delete storage[removeIndex];
+    insertIndex --;
+    return backElement;
   };
 
   someInstance.size = function(){
+    var count = 0;
+    for (var key in storage) {
+      count++;
+    }
+    console.log(storage);
+    return count;
   };
 
   return someInstance;
