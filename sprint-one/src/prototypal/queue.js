061753ab9e930cb12aef413ac861614c6601cd9f
cynthiaechen <cynthiaechen@gmail.com>
2015-08-19 21:47:00 -0700

    Made prototypal stack and queue pass

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.insertIndex = 0;
+  someInstance.removeIndex = 0;
+
+  return someInstance;
 };
 
-var queueMethods = {};
+
+var queueMethods = {
+  enqueue : function(value) {
+    this.storage[this.insertIndex] = value;
+    this.insertIndex++;
+  },
+  dequeue: function() {
+    var frontElement = this.storage[this.removeIndex];
+    delete this.storage[this.removeIndex];
+    this.removeIndex++;
+    return frontElement;
+  },
+  size: function() {
+    var count = 0;
+    for (var key in this.storage) {
+      count++;
+    }
+    return count;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.insertIndex = 0;
+
+  return someInstance;
 };
 
-var stackMethods = {};
 
-
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.insertIndex] = value;
+    this.insertIndex ++;
+  },
+  pop: function() {
+    var removeIndex = this.size() - 1;
+    var backElement = this.storage[removeIndex];
+    delete this.storage[removeIndex];
+    this.insertIndex --;
+    return backElement;
+  },
+  size: function() {
+    var count = 0;
+    for (var key in this.storage) {
+      count++;
+    }
+    return count;
+  }
+};
