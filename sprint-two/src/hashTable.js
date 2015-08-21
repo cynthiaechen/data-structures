12efb61f33333c889391c131b0acce1a2db3cc7f
cynthiaechen <cynthiaechen@gmail.com>
2015-08-21 16:07:46 -0700

    Implement hashTable in pseudoclassical style

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,21 +3,51 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value){
+  var hashNumber = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage.get(hashNumber);
+  if (bucket === undefined) {
+    this._storage.set(hashNumber, [[key, value]]);
+  } else {
+    var flag = false;
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket[i][1] = value;
+        flag = true
+      }
+    }
+  }
+  if (flag === false) {
+    bucket.push([key, value]);
+  }
 };
 
-HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-
+HashTable.prototype.retrieve = function(key){
+  var hashNumber = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage.get(hashNumber);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === key) {
+      return bucket[i][1];
+    }
+  }
 };
 
-HashTable.prototype.remove = function(k){
-
+HashTable.prototype.remove = function(key){
+  var hashNumber = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage.get(hashNumber);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === key) {
+      bucket[i][1] = null;
+    }
+  }
 };
+
+
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
