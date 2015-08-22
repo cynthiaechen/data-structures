59a96eae7062a75596c14e2d2012bab7ff97b85a
cynthiaechen <cynthiaechen@gmail.com>
2015-08-21 21:15:03 -0700

    Make Progress on BinarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,64 @@
 var BinarySearchTree = function(value){
+
+  this.value = value;
+  this.left = null;
+  this.right = null;
+
 };
+
+BinarySearchTree.prototype.insert = function(nodeValue) {
+  if (nodeValue < this.value) {
+    if (this.left) {
+      this.left.insert(nodeValue);
+    } else {
+      this.left = new BinarySearchTree(nodeValue);
+    }
+  } else if (nodeValue > this.value) {
+    if (this.right) {
+      this.right.insert(nodeValue);
+    } else {
+      this.right = new BinarySearchTree(nodeValue);
+    }
+  }
+  // if (nodeValue < this.value) {
+  //   this.left = new BinarySearchTree(nodeValue);
+  // } else {
+  //   this.right = new BinarySearchTree(nodeValue);
+  // }
+}
+
+BinarySearchTree.prototype.contains = function(nodeValue) {
+
+}
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
+}
+
+
+
+
+// }if (this.left) {
+//   if (nodeValue > this.left.value) {
+//     this.left.right.value = new BinarySearchTree(nodeValue)
+//   }
+
+// if (this.left === null) {
+    
+//   this.left = new BinarySearchTree(nodeValue);
+// } 
+// if (this.right === null) {
+//   this.right = new BinarySearchTree(nodeValue);
+// }
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+
+
+
+
